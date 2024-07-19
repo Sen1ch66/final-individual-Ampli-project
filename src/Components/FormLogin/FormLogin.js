83399fd3b358f2e91614e071c879c2fc80a212c0
Sen1ch66 <arsenzahovajko@gmail.com>
2024-07-19 18:39:37 +0200

    MVP2.0

--- src/Components/FormLogin/FormLogin.js
+++ src/Components/FormLogin/FormLogin.js
@@ -14,20 +14,6 @@ function FormLogin(props) {
             openForm(false)
         }
     }
-    const loginRequest = (e) => {
-        spinner.current.style.opacity = 1;
-        e.target.disabled = true;
-        fetch("https://jsonplaceholder.typicode.com/users/10")
-            .then((response) => response.json())
-            .then((json) => {
-                console.log(json);
-                spinner.current.style.opacity = 0;
-                e.target.disabled = false;
-                UserState.setUserData(json)
-                isLoginState.setIsLogin()
-                openForm(false)
-            });
-    };
     return (
         <div className='Background' onClick={onWrongClick}>
             <form>
